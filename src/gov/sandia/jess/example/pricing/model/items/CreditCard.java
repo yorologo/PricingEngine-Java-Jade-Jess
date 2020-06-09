137bf1d37d0301717ee2842f289fbcd5ef8948eb
yorologo <esaud@hotmail.es>
2020-06-09 12:22:53 -0500

    Cmabios en la clase CreditCard

--- src/gov/sandia/jess/example/pricing/model/items/CreditCard.java
+++ src/gov/sandia/jess/example/pricing/model/items/CreditCard.java
@@ -2,13 +2,19 @@ package gov.sandia.jess.example.pricing.model.items;
 
 public class CreditCard {
     private final String bank;
+    private final String type;
 
-    public CreditCard(String bank) {
+    public CreditCard(String bank, String type) {
         this.bank = bank;
+        this.type = type;
     }
 
     public String getBank() {
         return bank;
+    }
+
+    public String getType() {
+        return type;
     }
     
 }
