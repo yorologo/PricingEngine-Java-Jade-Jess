87bba580d98ab5de8638be4a303a27b7112c2d67
yorologo <esaud@hotmail.es>
2020-06-15 12:49:05 -0500

    Simple ejecucion del HandsOn11

--- src/gov/sandia/jess/example/pricing/demo/DemoDatabase.java
+++ src/gov/sandia/jess/example/pricing/demo/DemoDatabase.java
@@ -38,34 +38,35 @@ public class DemoDatabase implements Database {
 
 		Customer customer = new Customer(6);
 		ArrayList orderItems = new ArrayList();
-		orderItems.add(new OrderItem("CD Writer", 1234, 199.99f, 1));
-		orderItems.add(new OrderItem("AA Batteries", 4323, 4.99f, 2));
+
+		orderItems.add(new OrderItem("CD Writer", 1, 199.99f, 1));
+		orderItems.add(new OrderItem("AA Batteries", 3, 4.99f, 2));
 		orders.put(new Integer(123), new Order(orderItems, customer, new CreditCard("", "")));
 
 		customer = new Customer(1);
 		orderItems = new ArrayList();
-		orderItems.add(new OrderItem("Gold-tipped cable", 9876, 19.99f, 4));
+		orderItems.add(new OrderItem("Gold-tipped cable", 4, 19.99f, 4));
 		orders.put(new Integer(567), new Order(orderItems, customer, new CreditCard("", "")));
 
 		customer = new Customer(1);
 		orderItems = new ArrayList();
-		orderItems.add(new OrderItem("Incredibles DVD", 222123, 29.99f, 1));
+		orderItems.add(new OrderItem("Incredibles DVD", 6, 29.99f, 1));
 		orders.put(new Integer(666), new Order(orderItems, customer, new CreditCard("", "")));
 
 		// Customers HandsOn11
 		customer = new Customer(1);
 		orderItems = new ArrayList();
-		orderItems.add(new OrderItem(new Smartphone("iPhone 11 Pro", "Apple"), 41165, 4200.99f, 1));
+		orderItems.add(new OrderItem(new Smartphone("iPhone 11 Pro", "Apple"), 7, 4200.99f, 1));
 		orders.put(new Integer(415), new Order(orderItems, customer, new CreditCard("Banamex", "Bank issued")));
 
 		customer = new Customer(1);
 		orderItems = new ArrayList();
-		orderItems.add(new OrderItem(new Smartphone("Note 11", "Samsung"), 112647, 2600.99f, 1));
+		orderItems.add(new OrderItem(new Smartphone("Note 11", "Samsung"), 8, 2600.99f, 1));
 		orders.put(new Integer(125), new Order(orderItems, customer, new CreditCard("Liverpool VISA", "Bank issued")));
 
 		customer = new Customer(1);
 		orderItems = new ArrayList();
-		orderItems.add(new OrderItem(new Computer("MacBook Air", "apple"), 28355, 1700.99f, 1)); 
+		orderItems.add(new OrderItem(new Computer("MacBook Air", "apple"), 9, 1700.99f, 1)); 
 		orders.put(new Integer(181), new Order(orderItems, customer, new CreditCard("", "")));
 	}
 
